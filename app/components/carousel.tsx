0b524ab4e3ff5068cae0629d30c968c9edb80094
KadirZukorlic <kadirzukorlic@gmail.com>
2025-05-08 18:41:55 +0200

    Delete navigation import

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -4,7 +4,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 
 import "swiper/css";
-import "swiper/css/navigation";
 import "swiper/css/autoplay";
 
 const image1 = "/images/hero.jpg";
