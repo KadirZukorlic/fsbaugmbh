3c4b977dba1148937617b423bbc2265e5d5c2947
KadirZukorlic <kadirzukorlic@gmail.com>
2025-05-08 18:56:06 +0200

    Return navigation import

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 
 import "swiper/css";
+import "swiper/css/navigation";
 import "swiper/css/autoplay";
 
 const image1 = "/images/hero.jpg";
